2e3b4d881f5181050271900d4750b5f14304a9c4
Zephyr Zhang <zephyr1125@gmail.com>
2018-11-30 18:01:45 +0800

    完成传统版,30帧800人

---
+++ Assets/Classic/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.forward * GameManager.GM.charSpeed * Time.deltaTime;
+
+		if (pos.x < GameManager.GM.leftBound)
+		{
+			pos.x = GameManager.GM.rightBound;
+		}
+
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/CharCountText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharCountText : MonoBehaviour
+{
+
+	public Text Text;
+	
+	public void UpdateCount(int charCount)
+	{
+		Text.text = ""+charCount;
+	}
+}
