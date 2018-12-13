3f59ab6080d263de48c68641054884b4dad5efbc
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-13 20:34:31 +0800

    建立测试环境

---
+++ Assets/Compair/ECS/Tests/CharMoveSystemTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Entities.Tests;
+using Unity.Mathematics;
+
+public class CharMoveSystemTest
+{
+
+    private EntityManager _entityManager;
+    
+    [SetUp]
+    public void Init()
+    {
+        _entityManager = new EntityManager();
+    }
+
+    [Test]
+    public void Speed0_DontMove()
+    {
+        var entity = _entityManager.CreateEntity(typeof(Char), typeof (PositionHybrid));
+        _entityManager.SetComponentData(entity, new Char());
+//        _entityManager.SetComponentData(entity, new PositionHybrid{Value = float3.zero});
+//            
+//        // act
+//        World.CreateManager<CharMoveSystem>().Update();
+//            
+//        // assert
+//        Assert.AreEqual(new float3(0, 0, 0), _entityManager.GetComponentData<Position>(entity).Value);
+    }
+}
