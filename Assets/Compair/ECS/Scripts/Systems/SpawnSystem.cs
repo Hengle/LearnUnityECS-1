fd60efadfc36a54390a5d37290a67705e86e991a
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-14 15:16:56 +0800

    完成了一个最简单的ComponentSystem产生Entity的测试

---
+++ Assets/Compair/ECS/Scripts/Systems/SpawnSystem.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 纯粹为了学习测试系统写的Pure ECS
+ */
+
+using Unity.Entities;
+using Unity.Jobs;
+
+public struct Empty : IComponentData
+{
+    
+}
+
+public class SpawnSystem : ComponentSystem
+{
+    
+    protected override void OnUpdate()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        var entity = entityManager.CreateEntity();
+        entityManager.AddComponentData(entity, new Empty());
+    }
+}
---
+++ Assets/Compair/ECS/Tests/SpawnSystemTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SpawnSystemTest : ECSTestBase
+{
+    
+    [Test]
+    public void Spawn1Entity()
+    {
+        SpawnSystem ss = World.GetOrCreateManager<SpawnSystem>();
+        
+        ss.Update();
+
+        var entityManager = World.GetOrCreateManager<EntityManager>();
+        Assert.AreEqual(1, entityManager.GetAllEntities().Length);
+    }
+    
+    [TearDown]
+    public void TearDown() 
+    {
+       
+    }
+}
