0f405d9c1c6b43cffe36204b0e1b8bde0709ab7a
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-14 21:17:28 +0800

    完成Hybrid版本的单纯Spawn系统

--- Assets/Compair/ECS/Tests/CharMoveSystemTest.cs
+++ Assets/Compair/ECS/Tests/CharMoveSystemTest.cs
@@ -1,29 +1,32 @@
-﻿using NUnit.Framework;
-using Unity.Entities;
-using Unity.Mathematics;
-
-public class CharMoveSystemTest : ECSTestBase
-{
-
-    private EntityManager _entityManager;
-    
-    [SetUp]
-    public void Init()
-    {
-        _entityManager = new EntityManager();
-    }
-
-    [Test]
-    public void Speed0_DontMove()
-    {
-        var entity = _entityManager.CreateEntity(typeof(Char), typeof (PositionHybrid));
-        _entityManager.SetComponentData(entity, new Char());
-//        _entityManager.SetComponentData(entity, new PositionHybrid{Value = float3.zero});
+﻿//using NUnit.Framework;
+//using Unity.Entities;
+//using Unity.Mathematics;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+//
+//public class CharMoveSystemTest : ECSTestBase
+//{
+//    
+//    [Test]
+//    public void Speed0_DontMove()
+//    {
+//        var entity = new GameObject();
+//        entity.AddComponent<CharWrapper>();
+//        entity.AddComponent<PositionHybrid>();
+//        GameObjectEntity.AddToEntityManager(entityManager, entity);
 //            
 //        // act
-//        World.CreateManager<CharMoveSystem>().Update();
-//            
+//        var manager = World.GetOrCreateManager<CharMoveSystem>();
+//        
+//        Assert.AreEqual(new float3(0,0,0), entity.GetComponent<PositionHybrid>().Value);
+//        
+//        manager.Update();
+//        
 //        // assert
-//        Assert.AreEqual(new float3(0, 0, 0), _entityManager.GetComponentData<Position>(entity).Value);
-    }
-}
+//        //确实进入到system的监视列表中
+//        Assert.AreEqual(1, manager.CharGroup.CalculateLength());
+//        
+//        Assert.AreEqual(new float3(1, 1, 1), entity.GetComponent<PositionHybrid>().Value);
+////        Assert.AreEqual(new float3(1,1,1), manager.CharGroup.GetComponentArray<PositionHybrid>()[0].Value);
+//    }
+//}
