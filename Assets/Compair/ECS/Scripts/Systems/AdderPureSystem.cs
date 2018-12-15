35b83015e81e049eaff8f0141adbe6f5a67a5de1
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-15 13:23:00 +0800

    完成AdderPureSystem

---
+++ Assets/Compair/ECS/Scripts/Systems/AdderPureSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+public struct Adder : IComponentData
+{
+    public int Value;
+}
+
+public class AdderPureSystem : ComponentSystem
+{
+    public ComponentGroup AdderGroup;
+
+    protected override void OnCreateManager()
+    {
+        AdderGroup = GetComponentGroup(typeof(Adder));
+    }
+
+    protected override void OnUpdate()
+    {
+        var adders = AdderGroup.GetComponentDataArray<Adder>();
+        for (int i = 0; i < adders.Length; i++)
+        {
+            adders[i] = new Adder()
+            {
+                Value = adders[i].Value+1
+            };
+        }
+    }
+}
---
+++ Assets/Compair/ECS/Tests/AdderPureSystemTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AdderPureSystemTest : ECSTestBase
+{
+    
+    [Test]
+    public void AddOnceCorrect()
+    {
+        entityManager.CreateEntity(typeof(Adder));
+        
+        AdderPureSystem system = World.GetOrCreateManager<AdderPureSystem>();
+        
+        system.Update();
+
+        var adderGroup = entityManager.CreateComponentGroup(typeof(Adder));
+        var adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(1, adders.Length);
+        Assert.AreEqual(1, adders[0].Value);
+        
+        system.Update();
+        
+        Assert.AreEqual(1, adders.Length);
+        Assert.AreEqual(2, adders[0].Value);
+    }
+}
