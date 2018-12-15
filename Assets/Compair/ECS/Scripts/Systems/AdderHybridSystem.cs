663ae1e901dd424b57d233eb2d8bc9da3a47bf47
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-15 13:54:36 +0800

    完成AdderHybridSystem及其测试

---
+++ Assets/Compair/ECS/Scripts/Systems/AdderHybridSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+public class AdderWraper : ComponentDataWrapper<Adder>
+{
+}
+
+public class AdderHybridSystem : ComponentSystem
+{
+    public ComponentGroup AdderGroup;
+
+    protected override void OnCreateManager()
+    {
+        AdderGroup = GetComponentGroup(typeof(Adder));
+    }
+
+    protected override void OnUpdate()
+    {
+        var adders = AdderGroup.GetComponentDataArray<Adder>();
+        for (int i = 0; i < adders.Length; i++)
+        {
+            adders[i] = new Adder()
+            {
+                Value = adders[i].Value+1
+            };
+        }
+    }
+}
---
+++ Assets/Compair/ECS/Tests/AdderHybridSystemTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AdderHybridSystemTest : ECSTestBase
+{
+    
+    [Test]
+    public void AddOnceCorrect()
+    {
+        var go = new GameObject("test", typeof(AdderWraper));
+        
+        AdderHybridSystem system = World.GetOrCreateManager<AdderHybridSystem>();
+        
+        system.Update();
+
+        var adderGroup = entityManager.CreateComponentGroup(typeof(Adder));
+        var adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(1, adders.Length);
+        Assert.AreEqual(1, adders[0].Value);
+        
+        system.Update();
+        
+        Assert.AreEqual(1, adders.Length);
+        Assert.AreEqual(2, adders[0].Value);
+        
+        //用于创建entity的原型gameobject是被抛弃，不算在ECSWorld内的
+        Assert.AreEqual(0,go.GetComponent<AdderWraper>().Value.Value);
+    }
+}
