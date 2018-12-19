be97c70c24cff7dc6f6dd3e2f0656382005a7c60
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-19 15:39:50 +0800

    完成对AddJobProcessComponentDataSystem的测试

---
+++ Assets/Compair/ECS/Scripts/Systems/AddJobProcessComponentDataSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class AddJobProcessComponentDataSystem : JobComponentSystem
+{
+    public struct AddJob : IJobProcessComponentData<Adder>
+    {
+        public void Execute(ref Adder data)
+        {
+            data.Value = data.Value + 1;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new AddJob();
+        return job.Schedule(this, inputDeps);
+    }
+}
---
+++ Assets/Compair/ECS/Tests/AddJobProcessComponentDataTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AddJobProcessComponentDataTest : ECSTestBase
+{
+    
+    [TestCase(100)]
+    public void AddTimesCorrect(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            _entityManager.CreateEntity(typeof(Adder));
+        }
+        
+        AddJobProcessComponentDataSystem system = 
+            _world.GetOrCreateManager<AddJobProcessComponentDataSystem>();
+        
+        system.Update();
+        _entityManager.CompleteAllJobs();
+
+        var adderGroup = _entityManager.CreateComponentGroup(typeof(Adder));
+        var adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(100, adders.Length);
+        for (int i = 0; i < times; i++)
+        {
+            Assert.AreEqual(1, adders[i].Value);
+        }
+        
+        system.Update();
+        _entityManager.CompleteAllJobs();
+        
+        adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(100, adders.Length);
+        for (int i = 0; i < times; i++)
+        {
+            Assert.AreEqual(2, adders[i].Value);
+        }
+    }
+}
