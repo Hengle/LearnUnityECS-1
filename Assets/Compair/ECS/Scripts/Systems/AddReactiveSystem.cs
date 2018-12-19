2c43c54043873396338af2b1936779e3ab5248c4
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-19 16:27:37 +0800

    完成AddReactiveSystem及其测试

---
+++ Assets/Compair/ECS/Scripts/Systems/AddReactiveSystem.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class AddReactiveSystem : JobComponentSystem
+{
+    private ComponentGroup _adderGroup;
+
+    public struct AddJob : IJobParallelFor
+    {
+        public ComponentDataArray<Adder> Adders;
+        public void Execute(int i)
+        {
+            Adders[i] = new Adder()
+            {
+                Value = Adders[i].Value+1
+            };
+        }
+    }
+    
+    protected override void OnCreateManager()
+    {
+        _adderGroup = GetComponentGroup(typeof(Adder));
+        _adderGroup.SetFilterChanged(ComponentType.Create<Adder>());
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new AddJob()
+        {
+            Adders = _adderGroup.GetComponentDataArray<Adder>()
+        };
+        return job.Schedule(_adderGroup.CalculateLength(), 64, inputDeps);
+    }
+}
---
+++ Assets/Compair/ECS/Tests/AddReactiveSystemTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AddReactiveSystemTest : ECSTestBase
+{
+    
+    [TestCase(100)]
+    public void AddTimesCorrect(int times)
+    {
+        AddReactiveSystem system = _world.GetOrCreateManager<AddReactiveSystem>();
+        system.Update();
+        _entityManager.CompleteAllJobs();
+        
+        for (int i = 0; i < times; i++)
+        {
+            var entity = _entityManager.CreateEntity(typeof(Adder));
+            //从测试结果来看，对于ReactiveSystem,CreateEntity时是无法激活System的
+            //必须首次的SetComponentData才能激活
+            _entityManager.SetComponentData(entity, new Adder
+            {
+                Value = 0
+            });
+        }
+        
+        system.Update();
+        _entityManager.CompleteAllJobs();
+
+        var adderGroup = _entityManager.CreateComponentGroup(typeof(Adder));
+        var adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(100, adders.Length);
+        for (int i = 0; i < times; i++)
+        {
+            Assert.AreEqual(1, adders[i].Value);
+        }
+        
+        //再次Update不会再次激活ReactiveSystem运行
+        system.Update();
+        _entityManager.CompleteAllJobs();
+        
+        adders = adderGroup.GetComponentDataArray<Adder>();
+        Assert.AreEqual(100, adders.Length);
+        for (int i = 0; i < times; i++)
+        {
+            Assert.AreEqual(1, adders[i].Value);
+        }
+    }
+}
