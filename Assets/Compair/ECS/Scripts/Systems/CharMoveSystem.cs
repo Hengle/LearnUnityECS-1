72564755bcd423a01747f4c84b028078e021fb41
Zephyr Zhang <zephyr1125@gmail.com>
2018-12-19 16:44:13 +0800

    修复CharMoveSystem及其测试

--- Assets/Compair/ECS/Scripts/Systems/CharMoveSystem.cs
+++ Assets/Compair/ECS/Scripts/Systems/CharMoveSystem.cs
@@ -1,55 +1,55 @@
-﻿//using Unity.Burst;
-//using Unity.Collections;
-//using Unity.Entities;
-//using Unity.Jobs;
-//using Unity.Mathematics;
-//using Unity.Transforms;
-//using UnityEngine;
-//
-//public class CharMoveSystem : JobComponentSystem
-//{
-//    struct Move : IJobParallelFor
-//    {
-//        public ComponentArray<PositionHybrid> Positions;
-//        public float DeltaTime;
-//        public float3 MoveSpeed;
-//        public float xMin;
-//        public float xMax;
-//        
-//        public void Execute(int i)
-//        {
-//            var newPosition = new float3(
-//                Positions[i].Value + MoveSpeed * DeltaTime);
-//            if (newPosition.x <= xMin)
-//            {
-//                newPosition.x = xMax;
-//            }
-//            
-//            Positions[i].Value = newPosition;
-//        }
-//    }
-//
-//    public ComponentGroup CharGroup;
-//
-//    protected override void OnCreateManager()
-//    {
-//        CharGroup = GetComponentGroup(ComponentType.ReadOnly<Char>(),
-//            typeof(PositionHybrid));
-//    }
-//
-//    protected override JobHandle OnUpdate(JobHandle inputDeps)
-//    {
-//        var settings = Settings.Instance;
-//        var moveJob = new Move
-//        {
-//            Positions = CharGroup.GetComponentArray<PositionHybrid>(),
-//            DeltaTime = 1,
-//            MoveSpeed = 1,
-//            xMin = 1,
-//            xMax = 1
-//        };
-//        var moveJobHandle = moveJob.Schedule(CharGroup.CalculateLength(),
-//            64, inputDeps);
-//        return moveJobHandle;
-//    }
-//}
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CharMoveSystem : JobComponentSystem
+{
+    struct Move : IJobParallelFor
+    {
+        public ComponentArray<PositionHybrid> Positions;
+        public float DeltaTime;
+        public float3 MoveSpeed;
+        public float xMin;
+        public float xMax;
+        
+        public void Execute(int i)
+        {
+            var newPosition = new float3(
+                Positions[i].Value + MoveSpeed * DeltaTime);
+            if (newPosition.x <= xMin)
+            {
+                newPosition.x = xMax;
+            }
+            
+            Positions[i].Value = newPosition;
+        }
+    }
+
+    public ComponentGroup CharGroup;
+
+    protected override void OnCreateManager()
+    {
+        CharGroup = GetComponentGroup(ComponentType.ReadOnly<Char>(),
+            typeof(PositionHybrid));
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var settings = Settings.Instance;
+        var moveJob = new Move
+        {
+            Positions = CharGroup.GetComponentArray<PositionHybrid>(),
+            DeltaTime = 1,
+            MoveSpeed = 1,
+            xMin = 1,
+            xMax = 1
+        };
+        var moveJobHandle = moveJob.Schedule(CharGroup.CalculateLength(),
+            64, inputDeps);
+        return moveJobHandle;
+    }
+}
--- Assets/Compair/ECS/Tests/CharMoveSystemTest.cs
+++ Assets/Compair/ECS/Tests/CharMoveSystemTest.cs
@@ -1,32 +1,36 @@
-﻿//using NUnit.Framework;
-//using Unity.Entities;
-//using Unity.Mathematics;
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
-//
-//public class CharMoveSystemTest : ECSTestBase
-//{
-//    
-//    [Test]
-//    public void Speed0_DontMove()
-//    {
-//        var entity = new GameObject();
-//        entity.AddComponent<CharWrapper>();
-//        entity.AddComponent<PositionHybrid>();
-//        GameObjectEntity.AddToEntityManager(entityManager, entity);
-//            
-//        // act
-//        var manager = World.GetOrCreateManager<CharMoveSystem>();
-//        
-//        Assert.AreEqual(new float3(0,0,0), entity.GetComponent<PositionHybrid>().Value);
-//        
-//        manager.Update();
-//        
-//        // assert
-//        //确实进入到system的监视列表中
-//        Assert.AreEqual(1, manager.CharGroup.CalculateLength());
-//        
-//        Assert.AreEqual(new float3(1, 1, 1), entity.GetComponent<PositionHybrid>().Value);
-////        Assert.AreEqual(new float3(1,1,1), manager.CharGroup.GetComponentArray<PositionHybrid>()[0].Value);
-//    }
-//}
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharMoveSystemTest : ECSTestBase
+{
+    
+    [Test]
+    public void Speed1_Move()
+    {
+        var entity = new GameObject();
+        entity.AddComponent<GameObjectEntity>();
+        entity.AddComponent<CharWrapper>();
+        entity.AddComponent<PositionHybrid>();
+        GameObjectEntity.AddToEntityManager(_entityManager, entity);
+            
+        // act
+        var system = _world.GetOrCreateManager<CharMoveSystem>();
+        
+        system.Update();
+        _entityManager.CompleteAllJobs();
+        
+        // assert
+        //确实进入到system的监视列表中
+        Assert.AreEqual(1, system.CharGroup.CalculateLength());
+        
+        //手动创建的gameobject与由他产生的entity确实是关联的，
+        Assert.AreSame(entity.GetComponent<PositionHybrid>(),
+            system.CharGroup.GetComponentArray<PositionHybrid>()[0]);
+        Assert.AreEqual(new float3(1,1,1), entity.GetComponent<PositionHybrid>().Value);
+        Assert.AreEqual(new float3(1,1,1),
+            system.CharGroup.GetComponentArray<PositionHybrid>()[0].Value);
+    }
+}
